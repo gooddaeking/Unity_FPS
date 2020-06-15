b790872757550504faad6fcb08495ad5528d3f89
gooddaeking <gooddaeking@gmail.com>
2020-06-15 15:51:40 +0900

    프로젝트 생성
    - 플레이어 생성, 움직임, 충돌처리 추가

---
+++ New Unity FPS/Assets/_HJW/Scripts/CamFollow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    //플레이어에 카메라를 붙여서 이동해도 되지만
+    //드라마틱한 연출이 필요한 경우에 타겟을 따라다니도록 하는게 쉽지않다
+    //순간이동이 아닌 꼬랑지가 따라다니는 느낌으로 하는 효과도 연출할 수 있다
+    //카메라가 따라다닐 오브젝트
+    public Transform target;    //플레이어를 따라다닐거다
+    public Transform third;
+    public float followSpeed = 10.0f;
+    bool fps = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //카메라 위치를 강제로 타겟위치에 고정해둔다
+        //transform.position = target.position;
+
+        FollowTarget();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            fps = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            fps = false;
+        }
+    }
+
+    private void FollowTarget()
+    {
+        if (fps)
+        {
+            //타겟 방향 구하기 (백터의 뺄셈)
+            //방향 = 타겟 - 자기자신
+            Vector3 dir = target.position - transform.position;
+            //dir.Normalize(); // 노멀라이즈해주지 않으면 순간이동
+            transform.Translate(dir * followSpeed * Time.deltaTime);
+
+            //문제점 : 덜덜 떨림
+            if (Vector3.Distance(transform.position, target.position) < 2.0f)
+            {
+                transform.position = target.position;
+            }
+        }
+        else
+        {
+            //타겟 방향 구하기 (백터의 뺄셈)
+            //방향 = 타겟 - 자기자신
+            Vector3 dir = third.position - transform.position;
+            //dir.Normalize(); // 노멀라이즈해주지 않으면 순간이동
+            transform.Translate(dir * followSpeed * Time.deltaTime);
+
+            //문제점 : 덜덜 떨림
+            if (Vector3.Distance(transform.position, third.position) < 2.0f)
+            {
+                transform.position = third.position;
+            }
+        }
+    }
+}
---
+++ New Unity FPS/Assets/_HJW/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    public float moveSpeed = 10.0f;
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        //Vector3 Dir = (Vector3.forward * v) + (Vector3.right * h);
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();    //대각선 이동 속도를 상화좌우 속도와 동일하게 만들기
+        //게임에 따라 변동될 수 있음
+        //transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        dir = Camera.main.transform.TransformDirection(dir);
+        //transform.Translate(dir * moveSpeed * Time.deltaTime);
+        //심각한 문제 : 하늘 날아다님, 땅 뚫음, 충돌처리 안됨
+        
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+    }
+}
